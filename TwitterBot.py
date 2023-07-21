eaf9fe81b094507e95dc7c5d915b3022f1263f3c
Ari Abrams <ariabrams65@gmail.com>
2023-07-21 00:50:35 -0700

    Added ability to send tweets

---
+++ TwitterBot.py
@@ -0,0 +1,16 @@
+import tweepy
+import os
+
+consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+access_token = os.environ['TWITTER_ACCESS_TOKEN']
+access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+def send_tweet(text):
+    client = tweepy.Client(
+        consumer_key=consumer_key, consumer_secret=consumer_secret,
+        access_token=access_token, access_token_secret=access_token_secret
+    )
+    response = client.create_tweet(text=text)
+
+send_tweet('test2')
