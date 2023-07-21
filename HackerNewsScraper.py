328ff9d1fb8bc004204247a9171ef465210d3649
Ari Abrams <ariabrams65@gmail.com>
2023-07-21 02:13:29 -0700

    Added ability to filter posts by min points

--- HackerNewsScraper.py
+++ HackerNewsScraper.py
@@ -6,18 +6,20 @@ import time
 
 def get_post(athing, subline, url):
     post = {}
+    pattern = re.compile(r'\d+')
     titleline = athing.find(class_='titleline')
-    title = titleline.find('a', class_=False)
-    post['title'] = title.string
-    post['link'] = title['href']
     sitestr_element  = titleline.find(class_='sitestr')
-    post['source'] = None if not sitestr_element else sitestr_element.string
-    post['id'] = athing['id']
-    post['points'] = subline.find(class_='score').string
-    post['time'] = subline.find(class_='age')['title']
     comments = subline.contents[-2]
+    title = titleline.find('a', class_=False)
+
+    post['title'] = str(title.string)
+    post['link'] = title['href']
+    post['source'] = None if not sitestr_element else str(sitestr_element.string)
+    post['id'] = athing['id']
+    post['points'] = pattern.match(str(subline.find(class_='score').string)).group()
+    post['time'] = subline.find(class_='age')['title']
     post['comments_link'] = url + comments['href']
-    post['num_comments'] = '0' if comments.string == 'discuss' else re.compile(r'\d+').match(comments.string).group()
+    post['num_comments'] = '0' if comments.string == 'discuss' else pattern.match(str(comments.string)).group()
 
     return post
 
@@ -28,7 +30,7 @@ def get_soup(url, contentType, page_num):
     return soup
 
 
-def get_posts(contentType='news'):
+def get_posts(contentType='news', min_points=0):
     url = 'https://news.ycombinator.com/'
     posts = []
     page_num = 1
@@ -45,8 +47,9 @@ def get_posts(contentType='news'):
         page_num += 1
         time.sleep(1)
     
-    return posts
+    return [post for post in posts if int(post['points']) >= min_points] 
 
 
 if __name__ == '__main__':
-    posts = get_posts()
+    posts = get_posts(min_points=100)
+    print(json.dumps(posts, indent=4))
